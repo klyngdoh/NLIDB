c66661eda964a15224aa7f35f1547819702a6265
Suhas Mohan <mohan.su@husky.neu.edu>
2019-04-23 12:10:06 -0700

    Read input from user

--- src/main.py
+++ src/main.py
@@ -4,19 +4,37 @@ import pymysql.cursors
 connection = pymysql.connect(host='localhost',
                              user='root',
                              password='',
-                             db='NLIDB',
+                             db='nlp',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
-if __name__ == '__main__':
-    try:
 
+def fetchOneResult(query, params):
+    try:
         with connection.cursor() as cursor:
             # Read a single record
-            sql = "SELECT * FROM `orders` WHERE `customer_name`=%s"
-            cursor.execute(sql, ('KD',))
-            results = cursor.fetchall()
-            for result in results:
-                print(result)
-    finally:
-        connection.close()
+            sql = query
+            cursor.execute(sql, params)
+            result = cursor.fetchone()
+            return result
+    except:
+        print("An exception occured")
+
+
+if __name__ == '__main__':
+
+    print("Welcome to NLIDB!")
+    customer_email = input("Please enter your email: ")
+
+    user_record = fetchOneResult("Select * from `orders` where `customer_email`=%s", (customer_email,))
+
+    while user_record is None:
+        print("User record not found!")
+        customer_email = input("Please enter correct email: ")
+
+        user_record = fetchOneResult("Select * from `orders` where `customer_email`=%s", (customer_email,))
+    print("Welcome", user_record['customer_first'])
+    query = input("Enter your query: ")
+    while query != "exit":
+        print("Processing query", query)
+        query = input("Enter your query: ")
